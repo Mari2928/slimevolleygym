e60b14211922153cb15f16a2ff7a7749515cb5aa
Mari <charmee2928@gmail.com>
2025-03-28 13:56:45 +0000

    Update train_neat.py

--- training_scripts/train_neat.py
+++ training_scripts/train_neat.py
@@ -8,6 +8,7 @@ import pickle
 import slimevolleygym
 from slimevolleygym.neat import NeatModel
 from slimevolleygym import multiagent_rollout as rollout
+from eval_agents import evaluate_multiagent
 from visualize_network import*
 import visualize
 import matplotlib.pyplot as plt
@@ -17,7 +18,7 @@ random_seed = 612
 total_tournaments = 1 
 
 # Log results
-logdir = "neat_bl_p500_multiacts0.5_centerforehead_k10" 
+logdir = "results/neat_bl_p500_multiacts0.5_op2_gen100" 
 if not os.path.exists(logdir):
   os.makedirs(logdir)
 
@@ -33,15 +34,8 @@ np.random.seed(random_seed)
 class ScoreCompute():
     
   def __init__(self):
-    self.test_episodes = []
     self.generation = 0
-
-    self.episode_score = []
-    self.episode_length = []
-    self.episode_fitness = []
-
     self.history = []
-    self.gen_best_left = None
     self.gen_best = None
     self.solved = False
 
@@ -68,19 +62,15 @@ class ScoreCompute():
     scores = []
     lengths = []
     fitnesses = []
-    if self.generation % 100 == 0:     
-      for k in range(1000):
-        score, step, fitness = rollout(env, policy_right, policy_left)
-        sc.episode_score.append(score)
-        sc.episode_length.append(step)
-        sc.episode_fitness.append(fitness)
-        scores.append(score)
-        lengths.append(step) 
-        fitnesses.append(fitness)
-      if np.mean(scores) > 0:
+    if self.generation % 10 == 0:   
+      history = evaluate_multiagent(env, policy_right, policy_left)
+      score_ave = np.round(np.mean(history), 3)
+      print("neat scored", score_ave, "±", np.round(np.std(history), 3), "vs baseline", "over", "1000 trials.")
+  
+      if score_ave > 1.4:
         sc.solved = True
 
-      # training finished. save and visualize the checkpoint genome
+      # save the checkpoint genome
       model_filename = os.path.join(logdir, str(self.generation) +".pckl")
       f = open(model_filename, 'wb')
       pickle.dump(best_genome, f)
@@ -88,34 +78,29 @@ class ScoreCompute():
     else:
       for k in range(100):
         score, step, fitness = rollout(env, policy_right, policy_left)
-        sc.episode_score.append(score)
-        sc.episode_length.append(step)
-        sc.episode_fitness.append(fitness)
         scores.append(score)
         lengths.append(step)  
         fitnesses.append(fitness)     
-    print("generation: ",self.generation, 
-          "mean_score: ", np.mean(scores), 
-          "score_std: ", np.round(np.std(scores), 3), 
-          "mean_step", np.mean(lengths),
-          "mean_fitness:", np.round(np.mean(fitnesses), 3),
-          "solved:", sc.solved) 
+      print("generation: ",self.generation, 
+            "mean_score: ", np.mean(scores), 
+            "score_std: ", np.round(np.std(scores), 3), 
+            "mean_step", np.mean(lengths),
+            "mean_fitness:", np.round(np.mean(fitnesses), 3),
+            "solved:", sc.solved) 
      
     self.generation += 1
 
 if __name__ == '__main__':
   sc = ScoreCompute()
-  sc.gen_best = policy_right.p.run(sc.eval_genomes, 50)
+  sc.gen_best = policy_right.p.run(sc.eval_genomes, 100)
   # save visualized stats
-  visualize.plot_stats(policy_right.stats, ylog=False, view=True, filename=os.path.join(logdir, "fitness.svg"))
-  #plt.plot(sc.episode_score, 'g-', label='score')
-  plt.plot(sc.episode_fitness, 'b-', label='fitness')
+  visualize.plot_stats(policy_right.stats, ylog=False, view=True, filename=os.path.join(logdir, "fitness.svg")))
   plt.grid()
   plt.legend(loc='best')    
   plt.savefig(os.path.join(logdir, "scores.svg"))
   plt.close()
 
-  # save and visualize the winner
+  # save the best network
   model_filename = os.path.join(logdir, "neat_winner"+".pckl")
   f = open(model_filename, 'wb')
   pickle.dump(sc.gen_best, f)
